7f836655eaefd3a5f3b69ef6a2c32cff4daf5e68
Madheesh R <madheesh.r@cspl.com>
2025-10-28 18:12:44 +0530

    Sprint 0

---
+++ BankCustomerAPI/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿namespace BankManagementAPI.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+
+        public ICollection<UserRole> UserRoles { get; set; }
+        public ICollection<RolePermission> RolePermissions { get; set; }
+    }
+}
